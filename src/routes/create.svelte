f8148b8a039c0df8204e6af1f71ae5d522787304
leegakyeong <alaska7132@naver.com>
2020-10-24 23:39:18 +0900

    Add routes

---
+++ src/routes/create.svelte
@@ -0,0 +1,10 @@
+<p>그룹 관계를 선택하세요.</p>
+<form action="/group" method="post">
+  <input type="radio" id="family" name="relation" value="family" />
+  <label for="family">가족</label>
+  <input type="radio" id="friend" name="relation" value="friend" />
+  <label for="friend">친구</label>
+  <input type="radio" id="ice-breaking" name="relation" value="ice-breaking" />
+  <label for="ice-breaking">친해지고 싶어요</label>
+  <button type="submit">그룹 만들기</button>
+</form>
---
+++ src/routes/group.svelte
@@ -0,0 +1,15 @@
+<section>
+  <canvas></canvas>
+  <canvas></canvas>
+</section>
+<section>
+  <button>대화코드 복사하기</button>
+  <button>대화거리가 필요해</button>
+  <div>제일 싫어하는 음식이 무엇인가요?</div>
+</section>
+
+<style>
+  canvas {
+    border: 1px solid black;
+  }
+</style>

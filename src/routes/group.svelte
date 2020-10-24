c3410d023397bea8a675383edd189037c8fb8caa
leegakyeong <alaska7132@naver.com>
2020-10-25 00:13:51 +0900

    Reflec updated UI wireframe

--- src/routes/group.svelte
+++ src/routes/group.svelte
@@ -1,14 +1,25 @@
-<section>
-  <canvas></canvas>
-  <canvas></canvas>
-</section>
-<section>
-  <button>대화코드 복사하기</button>
-  <button>대화거리가 필요해</button>
-  <div>제일 싫어하는 음식이 무엇인가요?</div>
-</section>
+<main>
+  <section>
+    <canvas></canvas>
+    <canvas></canvas>
+  </section>
+  <section>
+    <button>대화코드 복사하기</button>
+    <div>
+      <p>대화거리가 필요해</p>
+      <div>
+        <button>선택형</button>
+        <button>질문형</button>
+      </div>
+      <div>제일 싫어하는 음식이 무엇인가요?</div>
+    </div>
+  </section>
+</main>
 
 <style>
+  main {
+    display: flex;
+  }
   canvas {
     border: 1px solid black;
   }

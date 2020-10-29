b29455d858eea227e5ae9b38e0bb9cae82fa11be
whatsbirddd <srpark2545@gmail.com>
2020-10-30 02:02:57 +0900

    modify abou.svelte

--- my-app/src/routes/about.svelte
+++ my-app/src/routes/about.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Box from './Box.svelte';
 	let groupcode ="";
 	function chatpage(){
 
@@ -7,36 +6,51 @@
 </script>
 
 <style>
-	box{
-		width:300px;
-		height:45px;
-		border: 3px;
-		border-color: #FFEEA5;
-		position : absolute;
-		top: 40%;
-		left:30%;
+	body{
+		height : 100vh;
+		margin: 0;
+		background: linear-gradient(#95cd7d 75%, #FFEEA5 25%);
 	}
-	button{
-		border: none;
+	h1,p{
+		text-align: center;
+		color: #ffeea5;
+		font-weight: 700;
+	}
+	input{
+		position: absolute;
+		top: 50%;
+		left:30%;
+		width: 300px;
+		height: 50px;
+		background-color: #95cd7d;
+		border: thick solid #ffeea5;
 	}
 	#join{
-		background-color: #FFEEA5;
-		color: #FFFFFF;
-		text-shadow : 1px,1px,1px,1px, #95cd7d;
-		padding : 15px 32px;
-		text-align: center;
-		display : inline-block;
-		font-size: 16px;
+		position: relative;
+		top: 50%;
+		left: 75%;
+		background-color: #ffeea5;
+		color: white;
+		text-shadow: 1px 1px 1px 1px #000000;
+		border:none;
+		width: 100px;
+		height: 50px;
+		font-size: large;
+		font-weight: 600;
 	}
 	#create{
-		background-color: #95cd7d;
-		color: #ffffff;
-		display : inline-block;
-		font-size: 16px;
-		padding: 15px 32px;
-		display : inline-block;
+		position : absolute;
+		left: 40%;
+		bottom: 20%;
+		background-color:#95cd7d;
+		border: none;
+		color:white;
+		font-size: large;
 		text-align: center;
+		width:200px;
+		height: 50px;
 	}
+	
 </style>
 
 <svelte:head>
@@ -44,10 +58,11 @@
 </svelte:head>
 
 <body>
-	<h1>Already in a group?</h1>
-	<p>Please enter the group code</p>
-	<box>
+	<div class = "green"> 
+		<h1>Already in a group?</h1>
+		<p>Please enter the group code</p>
 		<input value = {groupcode}>
-	</box><button id= "join" on:click = {chatpage}>JOIN</button>
+		<button id= "join" on:click = {chatpage}>JOIN</button>
+	</div>
 	<button id= "create">Create a group!</button>
 </body>
